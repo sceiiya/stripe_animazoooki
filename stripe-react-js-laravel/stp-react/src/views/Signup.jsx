5a82a019358292b1de157932e932e27a5fce4d92
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-07-28 23:34:22 +0800

    add signup structure

--- stripe-react-js-laravel/stp-react/src/views/Signup.jsx
+++ stripe-react-js-laravel/stp-react/src/views/Signup.jsx
@@ -2,6 +2,24 @@ const Signup = () => {
     return(
         <>
             <h1>def</h1>
+
+            <div>
+                <div>
+
+                    <div class="mb-3">
+                      <label for="" class="form-label">Email</label>
+                      <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="abc@mail.com">
+                      <small id="emailHelpId" class="form-text text-muted">Help text</small>
+                    </div>
+
+                    <div class="mb-3">
+                      <label for="" class="form-label">Password</label>
+                      <input type="password" class="form-control" name="" id="" placeholder="">
+                    </div>
+
+                </div>
+            </div>
+
         </>
     )
 }
