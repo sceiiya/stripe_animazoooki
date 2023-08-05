da9d7091926a163c0ccfaeacc40961c46dc85709
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-05 23:35:02 +0800

    add product factory details

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,9 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->text(),
+            'price' => fake()->randomFloat(2, 100, 1000),
+            'image' => fake()->imageUrl()
         ];
     }
 }
