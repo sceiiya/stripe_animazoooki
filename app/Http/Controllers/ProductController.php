a71fb936955c50d123bb6cd4ccaafce41ea90838
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-05 23:34:45 +0800

    add product controller index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
 }
