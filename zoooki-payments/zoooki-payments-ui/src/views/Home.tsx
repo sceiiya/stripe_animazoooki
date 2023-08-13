3c906ae4e11068fa56101109674e222ddd141954
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-08-13 20:11:36 +0800

    test ui section bleed

--- zoooki-payments/zoooki-payments-ui/src/views/Home.tsx
+++ zoooki-payments/zoooki-payments-ui/src/views/Home.tsx
@@ -8,10 +8,15 @@ const Home = () =>
             <meta name="description" content="Zooki collabs we sell anime merchandises and clothes partnered to our brand." />
             <link rel="canonical" href="/home" data-rh="true" />
         </Helmet>
-        <div>
+        <div className="marg">blop</div>
+        <div className="container meg">
             <h1>This is Homepage that you see</h1>
             <h2>Zoooki Stripe Collabs</h2>
+            <div className="sectionated full-bleed">
+                morg
+            </div>
         </div>
+        <div className="marg">blop</div>
     </>
 }
 
